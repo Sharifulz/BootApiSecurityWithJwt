4e06afb4d8e037e96b4fdf07c5ef28f50b3460df
Shariful Islam <shariful@ctrends-software.com>
2020-04-01 15:37:10 +0600

    db conn added

--- src/main/java/com/ctrends/config/MyUserDetailService.java
+++ src/main/java/com/ctrends/config/MyUserDetailService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class MyUserDetailService implements UserDetailsService {
 	 @Override
-	    public UserDetails loadUserByUsername(String userName){
-	        return new User("abc","123",new ArrayList<>());
-	    }
+	 public UserDetails loadUserByUsername(String userName){
+	        return new User("ert","789",new ArrayList<>());
+	 }
 }
