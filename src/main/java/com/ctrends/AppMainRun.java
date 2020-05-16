32db49a0b672d5c9b25bb2a16ef4b8db967bee96
Shariful Islam <shariful@ctrends-software.com>
2020-05-16 11:00:38 +0600

    Backup project as reinstalling system

--- src/main/java/com/ctrends/AppMainRun.java
+++ src/main/java/com/ctrends/AppMainRun.java
@@ -3,6 +3,9 @@ package com.ctrends;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * @author Shariful Islam
+ */
 @SpringBootApplication
 public class AppMainRun {
 
